368985e0b09935949250bdde60eaccf7cbe8296e
Nayan Khade <nayankhade2001@gmail.com>
2021-06-21 12:54:32 +0530

    Multiplication Table 1 to 20

---
+++ MultiplicationTable_1to20.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable_1to20 {
+    public static void main(String[] args) {
+
+        for (int j = 1; j <= 11; j++) {
+            for (int i = 1; i <= 20; i++) {
+                System.out.print(i * j + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
