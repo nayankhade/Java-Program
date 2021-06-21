6d6cd24f63d75166cb262366fb50019fb19b6815
Nayan Khade <nayankhade2001@gmail.com>
2021-06-22 00:45:03 +0530

    Matrix Addition

--- Arrays/Matrix_Addition.java
+++ Arrays/Matrix_Addition.java
@@ -1,34 +1,37 @@
+package Arrays;
+
 import java.util.Scanner;
 
 public class Matrix_Addition {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter Dimensions : ");
 
         int rows = sc.nextInt();
-        int cols=sc.nextInt();
+        int cols = sc.nextInt();
 
-        int a[][]=new int[rows][cols];
-        int b[][]=new int[rows][cols];
-        
+        int a[][] = new int[rows][cols];
+        int b[][] = new int[rows][cols];
+
         System.out.println("Enter Array a : ");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                a[i][j]=sc.nextInt();
+                a[i][j] = sc.nextInt();
             }
         }
         System.out.println("Enter Array b : ");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                b[i][j]=sc.nextInt();
+                b[i][j] = sc.nextInt();
             }
         }
-        int c[][]=new int[rows][cols];
+        /* Addition */
+        int c[][] = new int[rows][cols];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                c[i][j]=a[i][j]+b[i][j];
+                c[i][j] = a[i][j] + b[i][j];
             }
         }
         System.out.println("Result Array c is : ");
