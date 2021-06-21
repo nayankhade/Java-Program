1e10996edd2c4eda0bc5c5ca97076fda663f98c6
Nayan Khade <nayankhade2001@gmail.com>
2021-06-21 12:51:55 +0530

    Count Numbers of Digits

---
+++ Count_NumberofDigits.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Count_NumberofDigits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int numberOfDigits = (int) Math.log10(n) + 1;
+        System.out.println(numberOfDigits);
+    }
+}
